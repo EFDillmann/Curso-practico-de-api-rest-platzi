37fd23856589153308aac4208433df9be254f117
dillmanncode <dillmanncode1999@gmail.com>
2023-02-22 16:13:05 -0300

    Maquetación SPA

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 console.log(API_KEY);
+
+// Toggle menú
+const headerToggle = document.querySelector(".header-toggleIcon");
+const toggleLine1 = document.querySelector(".toggleIcon-line1");
+const toggleLine2 = document.querySelector(".toggleIcon-line2");
+const toggleLine3 = document.querySelector(".toggleIcon-line3");
+
+headerToggle.addEventListener("click", () => {
+    toggleLine1.classList.toggle("toggleIcon-line1Active");
+    toggleLine2.classList.toggle("toggleIcon-line2Active");
+    toggleLine3.classList.toggle("toggleIcon-line3Active");
+});
