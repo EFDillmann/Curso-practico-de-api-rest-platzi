6d9d8a4d13df8fb362b758c883877a4d186552c8
dillmanncode <dillmanncode1999@gmail.com>
2023-02-25 22:44:27 -0300

    generic list

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ const headerToggle = document.querySelector(".header-toggleIcon");
 const toggleLine1 = document.querySelector(".toggleIcon-line1");
 const toggleLine2 = document.querySelector(".toggleIcon-line2");
 const toggleLine3 = document.querySelector(".toggleIcon-line3");
+const asideMenu = document.querySelector("#asideMenu");
 
 headerToggle.addEventListener("click", () => {
     toggleLine1.classList.toggle("toggleIcon-line1Active");
     toggleLine2.classList.toggle("toggleIcon-line2Active");
     toggleLine3.classList.toggle("toggleIcon-line3Active");
+    asideMenu.classList.toggle("inactive");
 });
