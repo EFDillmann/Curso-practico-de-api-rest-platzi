841cefae80e61f143e328eda7eb0921fe23414d6
dillmanncode <dillmanncode1999@gmail.com>
2023-03-07 15:29:37 -0300

    dinamyc category page

--- src/main.js
+++ src/main.js
@@ -9,8 +9,40 @@ const api = axios.create({
     }
 });
 
-// Toggle menú
+// Utilities
+function createMovies (array, container) {
+    container.innerHTML = "";
 
+    array.forEach(movie => {
+        const movieContainer = document.createElement("div");
+        const movieContainerPoster = document.createElement("img");
+
+        movieContainer.classList.add("movie-container");
+        movieContainerPoster.setAttribute("src", `${genericPoster}${movie.poster_path
+        }`);
+        movieContainerPoster.setAttribute("alt", `${movie.title}`);
+
+        movieContainer.appendChild(movieContainerPoster);
+        container.appendChild(movieContainer);
+    })
+}
+function templateSlider (array, container, options = []) {
+    container.innerHTML = "";
+
+    container.innerHTML = `
+    <label id="selector-${options[0]}" for="item-${options[0]}" class="poster">
+        <img src=${genericPoster}${array[0].poster_path} alt=${array[0].title}>
+    </label>
+    <label id="selector-${options[1]}" for="item-${options[1]}" class="poster">
+        <img src=${genericPoster}${array[1].poster_path} alt=${array[1].title}>
+    </label>
+    <label id="selector-${options[2]}" for="item-${options[2]}" class="poster">
+        <img src=${genericPoster}${array[2].poster_path} alt=${array[2].title}>
+    </label>
+`;
+}
+
+// Toggle menú
 headerToggle.addEventListener("click", () => {
     toggleLine1.classList.toggle("toggleIcon-line1Active");
     toggleLine2.classList.toggle("toggleIcon-line2Active");
@@ -26,38 +58,40 @@ const getTrendingMoviesPreview = async () => {
     const { data } = await api(`trending/movie/week`);
     const movies = data.results;
 
-    trendingPreviewMoviesPosters.innerHTML = "";
+    templateSlider(movies, trendingPreviewMoviesPosters, [1,2,3]);
+//     trendingPreviewMoviesPosters.innerHTML = "";
 
-    trendingPreviewMoviesPosters.innerHTML = `
-    <label id="selector-1" for="item-1" class="poster">
-        <img src=${genericPoster}${movies[0].poster_path} alt=${movies[0].title}>
-    </label>
-    <label id="selector-2" for="item-2" class="poster">
-        <img src=${genericPoster}${movies[1].poster_path} alt=${movies[1].title}>
-    </label>
-    <label id="selector-3" for="item-3" class="poster">
-        <img src=${genericPoster}${movies[2].poster_path} alt=${movies[2].title}>
-    </label>
-`;
+//     trendingPreviewMoviesPosters.innerHTML = `
+//     <label id="selector-1" for="item-1" class="poster">
+//         <img src=${genericPoster}${movies[0].poster_path} alt=${movies[0].title}>
+//     </label>
+//     <label id="selector-2" for="item-2" class="poster">
+//         <img src=${genericPoster}${movies[1].poster_path} alt=${movies[1].title}>
+//     </label>
+//     <label id="selector-3" for="item-3" class="poster">
+//         <img src=${genericPoster}${movies[2].poster_path} alt=${movies[2].title}>
+//     </label>
+// `;
 }
 
 const getTrendingSeriesPreview = async () => {
     const { data } = await api(`trending/tv/day`);
     const series = data.results;
 
-    trendingPreviewSeriesPosters.innerHTML = "";
+    templateSlider(series, trendingPreviewSeriesPosters, [4,5,6]);
+//     trendingPreviewSeriesPosters.innerHTML = "";
 
-    trendingPreviewSeriesPosters.innerHTML = `
-    <label id="selector-4" for="item-4" class="poster">
-        <img src=${genericPoster}${series[0].poster_path} alt=${series[0].title}>
-    </label>
-    <label id="selector-5" for="item-5" class="poster">
-        <img src=${genericPoster}${series[1].poster_path} alt=${series[1].title}>
-    </label>
-    <label id="selector-6" for="item-6" class="poster">
-        <img src=${genericPoster}${series[2].poster_path} alt=${series[2].title}>
-    </label>
-`;
+//     trendingPreviewSeriesPosters.innerHTML = `
+//     <label id="selector-4" for="item-4" class="poster">
+//         <img src=${genericPoster}${series[0].poster_path} alt=${series[0].title}>
+//     </label>
+//     <label id="selector-5" for="item-5" class="poster">
+//         <img src=${genericPoster}${series[1].poster_path} alt=${series[1].title}>
+//     </label>
+//     <label id="selector-6" for="item-6" class="poster">
+//         <img src=${genericPoster}${series[2].poster_path} alt=${series[2].title}>
+//     </label>
+// `;
 }
 
 const getCategoriesPreview = async () => {
@@ -67,23 +101,29 @@ const getCategoriesPreview = async () => {
     asideMenuList.innerHTML = "";
 
     categories.forEach(category => {
-                // <li class="asideMenu-item"><a href="#">Action</a></li>
-                // <li class="asideMenu-item"><a href="#">Adventure</a></li>
-                // <li class="asideMenu-item"><a href="#">Comedy</a></li>
-                // <li class="asideMenu-item"><a href="#">Romance</a></li>
-                // <li class="asideMenu-item"><a href="#">Musical</a></li>
-                // <li class="asideMenu-item"><a href="#">Drama</a></li>
         const asideMenuItem = document.createElement("li");
-        const asideMenuLink = document.createElement("a");
+        const asideMenuLink = document.createElement("h4");
 
         asideMenuItem.classList.add("asideMenu-item");
         asideMenuItem.appendChild(asideMenuLink);
-        asideMenuLink.setAttribute("href", "#");
+        asideMenuLink.addEventListener("click", () => {
+            location.hash = `#category=${category.id}-${category.name}`;
+        });
         asideMenuLink.innerText=`${category.name}`;
 
         asideMenuList.appendChild(asideMenuItem);
     });
-
-
 }
 
+const getMovieByCategory = async (id) => {
+    const { data } = await api(`/discover/movie`, {
+        params: {
+            with_genres: id,
+        },
+    });
+    const movies = data.results;
+
+    // console.log(movies);
+
+    createMovies(movies, verticalListSection);
+}
